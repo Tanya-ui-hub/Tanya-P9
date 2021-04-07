1ef160a0cdd70d649a65457f0afa2f7862ce3fff
Tanya-ui-hub <tatyanapenzina92@gmail.com>
2021-04-07 13:21:38 +0300

    java mili

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticketPrice = 2000;
+        int mileBonus = 20;
+        long totalBonus = ticketPrice / mileBonus;
+        System.out.println(totalBonus);
+    }
+}
